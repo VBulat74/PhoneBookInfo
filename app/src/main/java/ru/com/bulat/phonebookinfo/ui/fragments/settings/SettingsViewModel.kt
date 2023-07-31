367aa282bb9fdbe97fd10d9ad63109b686fa32b0
VB <Vladimir.Bulat@gmail.com>
2023-07-31 15:45:55 +0300

    read csv file

--- app/src/main/java/ru/com/bulat/phonebookinfo/ui/fragments/settings/SettingsViewModel.kt
+++ app/src/main/java/ru/com/bulat/phonebookinfo/ui/fragments/settings/SettingsViewModel.kt
@@ -9,11 +9,15 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import ru.com.bulat.phonebookinfo.db.MainDataBase
 import ru.com.bulat.phonebookinfo.entities.NumberRangeItem
 import ru.com.bulat.phonebookinfo.utilits.Resource
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class SettingsViewModel (database: MainDataBase) : ViewModel() {
 
@@ -26,7 +30,35 @@ class SettingsViewModel (database: MainDataBase) : ViewModel() {
 
         dao.clearNumberRange()
 
+        // read the file
+        //val reader = Files.newBufferedReader(Paths.get(".number_range.csv"))
         val mInput = InputStreamReader(context.assets.open("number_range.csv"))
+        val reader = BufferedReader(mInput)
+        val csvParser = CSVParser(
+            reader,
+            CSVFormat.EXCEL
+                .withDelimiter(';')
+                .withRecordSeparator("\r\n")
+        )
+
+        for (csvRecord in csvParser) {
+            val number_range = NumberRangeItem (
+                id = null,
+                codeDef = csvRecord[0].toInt(),
+                number_begin = csvRecord[1].toLong(),
+                number_end = csvRecord[2].toLong(),
+                capacity = csvRecord[3].toLong(),
+                provider = csvRecord[4],
+                region = csvRecord[5].toInt(),
+                mnc = csvRecord[6].toInt(),
+                route = csvRecord[7]
+            )
+
+            Log.d("AAA", "Add $number_range")
+            dao.insertNumberRange(number_range)
+        }
+
+        /*val mInput = InputStreamReader(context.assets.open("number_range.csv"))
         val reader = BufferedReader(mInput)
 
         var line : String
@@ -53,7 +85,7 @@ class SettingsViewModel (database: MainDataBase) : ViewModel() {
             )
             Log.d("AAA", "Add $number_range")
             dao.insertNumberRange(number_range)
-        }
+        }*/
 
         numberRangeLiveData.postValue(Resource.Success(true))
     }
